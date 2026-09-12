240fa8d7ea1a8ea38ff1d233074a8e8cbc0f240c
Aaron Garcia <axyscreativeagency@gmail.com>
2026-09-12 14:40:44 -0600

    team page updates

--- src/static/javascript/components/cursor-content.js
+++ src/static/javascript/components/cursor-content.js
@@ -22,9 +22,9 @@ if (cursor) {
 
   const applySize = (size) => {
     iconEl.classList.remove(
-      "mouse-cursor__icon-sm",
-      "mouse-cursor__icon-md",
-      "mouse-cursor__icon-lg",
+      "mouse-cursor__icon--sm",
+      "mouse-cursor__icon--md",
+      "mouse-cursor__icon--lg",
     );
     iconEl.classList.add(`mouse-cursor__icon-${size}`);
   };
